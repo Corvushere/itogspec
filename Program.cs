64025d14c21d10c12b628d2d3652543a0e905ec0
Corvushere <corvushere@gmail.com>
2023-02-02 22:49:33 +0300

    new console

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array =
+{
+    "a",
+    "bb",
+    "ccc",
+    "dddd",
+    "eeeee",
+    "z"
+};
+ 
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+ 
+Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
