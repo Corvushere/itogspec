5007b1f9de62a6e1e46823b5033ab55f8fd437d0
Corvushere <corvushere@gmail.com>
2023-02-04 06:54:55 +0300

    fin

--- Program.cs
+++ Program.cs
@@ -1,38 +1,45 @@
 ﻿
+Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int size = n;
+string[] array = new string[n];
+FillArray(array);
+SortArray(array);
 
-    Console.WriteLine("Введите размер массива");
-    int n = Convert.ToInt32(Console.ReadLine());
-    int size = n;
-    string[] array = new string[n];
+void FillArray(string[] array)
+{
     for (int i = 0; i < n; i++)
     {
         Console.WriteLine("Введите " + (i + 1) + " элемент массива из " + n);
         array[i] = Console.ReadLine(); ;
     }
-Console.WriteLine("Исходный массив");
+    Console.WriteLine("Исходный массив");
     for (int i = 0; i < n; i++)
     {
-        
+
         Console.Write(array[i] + " ");
     }
     Console.WriteLine();
-
-var result = new string[array.Length];
-var realSize = 0;
-foreach (var value in array)
-{
-    if (value.Length <= 3)
-    {
-        result[realSize] = value;
-        realSize++;
-    }
 }
- Console.WriteLine("Отсортированный массив");
- for (int i = 0; i < array.Length; i++)
+
+void SortArray(string[] array)
+{
+
+    var result = new string[array.Length];
+    var realSize = 0;
+    foreach (var value in array)
     {
-        
+        if (value.Length <= 3)
+        {
+            result[realSize] = value;
+            realSize++;
+        }
+    }
+    Console.WriteLine("Отсортированный массив");
+    for (int i = 0; i < array.Length; i++)
+    {
+
         Console.Write(result[i] + " ");
     }
     Console.WriteLine();
-
- 
+}
